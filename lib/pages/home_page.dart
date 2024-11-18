f454394ad1bd2d91187551bcec87578ecbc003f7
mohzahidi <mohzahidi4@gmail.com>
2024-11-18 22:09:53 +0700

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,106 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:record/record.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final AudioRecorder audioRecorder = AudioRecorder();
+  final AudioPlayer audioPlayer = AudioPlayer();
+
+  String? recordingPath;
+  bool isRecording = false, isPlaying = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: _recordingButton(),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (recordingPath != null)
+            MaterialButton(
+              onPressed: () async {
+                if (audioPlayer.playing) {
+                  audioPlayer.stop();
+                  setState(() {
+                    isPlaying = false;
+                  });
+                } else {
+                  await audioPlayer.setFilePath(recordingPath!);
+                  audioPlayer.play();
+                  setState(() {
+                    isPlaying = true;
+                  });
+                }
+              },
+              color: Theme.of(context).colorScheme.primary,
+              child: Text(
+                isPlaying
+                    ? "Stop Playing Recording"
+                    : "Start Playing Recording",
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          if (recordingPath == null)
+            const Text(
+              "No Recording Found. :(",
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _recordingButton() {
+    return FloatingActionButton(
+      onPressed: () async {
+        if (isRecording) {
+          String? filePath = await audioRecorder.stop();
+          if (filePath != null) {
+            setState(() {
+              isRecording = false;
+              recordingPath = filePath;
+            });
+          }
+        } else {
+          if (await audioRecorder.hasPermission()) {
+            final Directory appDocumentsDir =
+                await getApplicationDocumentsDirectory();
+            final String filePath =
+                p.join(appDocumentsDir.path, "recording.wav");
+            await audioRecorder.start(
+              const RecordConfig(),
+              path: filePath,
+            );
+            setState(() {
+              isRecording = true;
+              recordingPath = null;
+            });
+          }
+        }
+      },
+      child: Icon(
+        isRecording ? Icons.stop : Icons.mic,
+      ),
+    );
+  }
+}
